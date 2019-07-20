d7a6c2c2235e0f4a1472cdb7d5c249b884628ab1
Outsider987 <t7902195204@gmail.com>
2019-07-20 16:59:00 +0800

    Make todoservice and make AddToDoItem function and annimation

--- ClientApp/src/app/main/main.component.ts
+++ ClientApp/src/app/main/main.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition,  } from '@angular/animations';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger('myInsertRemoveTrigger', [
+      transition(':enter', [
+        style({ right: '-100%' }),
+        animate('.5s', style({ right: 0 })),
+      ]),
+      transition(':leave', [
+        animate('.5s', style({ right: "-100%" }))
+      ])
+    ]),
+    trigger('changeValue', [
+      transition(':enter', [
+        style({ transform: 'translateX(-30%)', opacity: 0 }),
+        animate('.3s ease-out', style({ transform: 'translateX(0)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ position: 'absolute' }),
+        animate('.5s ease-out', style({ opacity: 0 })),
+      ]),
+    ])
+  ]
 })
 export class MainComponent implements OnInit {
-
+  public TodoText = "THE FIRST THING TODO TODAY";
   constructor() { }
 
   ngOnInit() {
   }
-
+  AddToDoItem(item) {
+    if(item.evalue =="") return;
+    let key = (new Date()).getTime();
+    console.log("testwork");
+  }
 }
